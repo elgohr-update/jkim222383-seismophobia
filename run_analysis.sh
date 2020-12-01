fdfc41fb840cc719b2e818bd449bcf139877861c
Dustin Andrews <dustin.brown.andrews@gmail.com>
2020-12-01 11:45:06 -0700

    Makefile working

--- run_analysis.sh
+++ run_analysis.sh
@@ -5,7 +5,7 @@
 python src/download_csv.py 'https://raw.githubusercontent.com/fivethirtyeight/data/master/san-andreas/earthquake_data.csv' data/raw/earthquake.csv
 
 # Rename columns, add target column
-Rscript src/pre_process_seismophobia.R --input_path=data/raw/earthquake_data.csv --out_dir=data/processed
+Rscript src/pre_process_seismophobia.R --input_path=data/raw/earthquake.csv --out_dir=data/processed
 
 # Build eda visuals 
 Rscript src/seismophobia_eda.R --data_path=data/processed/train.csv --out_dir=visuals
