45ca3abfdfa37d8cdfef34de8c908406ee88dc71
Dustin Andrews <dustin.brown.andrews@gmail.com>
2020-12-10 16:43:55 -0700

    Updates to run Docker scripts on Linux

--- clean_and_run_analysis_docker.sh
+++ clean_and_run_analysis_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -e PASSWORD="test" -v "/$(pwd)":/home/seismophobia/ dbandrews/seismophobia:latest make -C //home/seismophobia clean all
+docker run --rm -e PASSWORD="test" -v "/$(pwd)":/home/seismophobia/ dbandrews/seismophobia:latest make directory=/home/seismophobia clean all
